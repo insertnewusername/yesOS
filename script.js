21860cd22caecc03d2278e47065d39e6aecff7e1
IMMAHUMAN <yuchenliu.matthew@gmail.com>
2026-08-28 20:52:55 +1000

    make open and close button for welcomebar

--- script.js
+++ script.js
@@ -52,3 +52,28 @@ function dragElement(element) {
     document.onmousemove = null;
   }
 }
+
+
+
+/*Closing welcomescreen*/
+var welcomeScreen = document.querySelector("#welcome")
+
+var welcomeScreenClose = document.querySelector("#welcomeclose")
+
+var welcomeScreenOpen = document.querySelector("#welcomeopen")
+
+function closeWindow(element) {
+  element.style.display = "none"
+}
+
+function openWindow(element) {
+  element.style.display = "block"
+}
+
+welcomeScreenClose.addEventListener("click", function() {
+  closeWindow(welcomeScreen);
+});
+
+welcomeScreenOpen.addEventListener("click", function() {
+  openWindow(welcomeScreen);
+});
